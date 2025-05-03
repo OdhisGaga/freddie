9670346d1c16cc9308a550ba2520dce560f78679
Freezerfred1 <freezermtoxic@gmail.com>
2025-05-04 01:36:01 +0300

    Update freezer.js

--- wasiqr.js
+++ wasiqr.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Wasi_Tech,
+	default: Freezer_Tech,
 	useMultiFileAuthState,
 	jidNormalizedUser,
 	Browsers,
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function WASI_MD_QR_CODE() {
+	async function FREEZER_MD_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Wasi_Tech = Wasi_Tech({
+			let Qr_Code_By_Freezer_Tech = Freezer_Tech({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
+			Qr_Code_By_Freezer_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Freezer_Tech.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -58,35 +58,35 @@ router.get('/', async (req, res) => {
 				   let b64data = Buffer.from(data).toString('base64');
 				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
 	
-				   let TREX_MD_TEXT = `
-❏ ｇａｇａ ｍｄ ｓｅｓｓｉｏｎ ｓｕｃｃｅｓｓｆｕｌｌｙ ｃｏｎｎｅｃｔｅｄ ❏
+				   let FREEZER_MD_TEXT = `
+❏ freezer ｍｄ ｓｅｓｓｉｏｎ ｓｕｃｃｅｓｓｆｕｌｌｙ ｃｏｎｎｅｃｔｅｄ ❏
 
 ❏ ʏᴏᴜ ʜᴀᴠᴇ ᴄᴏᴍᴘʟᴇᴛᴇᴅ ᴛʜᴇ ғɪʀsᴛ sᴛᴇᴘ , ᴄᴏᴘʏ ᴛʜᴇ ᴀʙᴏᴠᴇ sᴇssɪᴏɴ ᴀɴᴅ sᴇɴᴅ ɪᴛ ᴛᴏ ʏᴏᴜʀ ᴅᴇᴘʟᴏʏᴇʀ ᴏʀ ᴅᴇᴘʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ❏
 
 ❏ғᴏʟʟᴏᴡ ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ғᴏʀ ᴜᴘᴅᴀᴛᴇs  ❏
-[ 🔘https://whatsapp.com/channel/0029VasnifMFi8xW4Mqysn2F🔘]
+[ 🔘https://whatsapp.com/channel/0029Vb5mxL6F1YlTPss80Y2t🔘]
 ❏ᴀɴʏ ᴅɪғғɪᴄᴜʟᴛɪᴇs ᴄᴏɴᴛᴀᴄᴛ ʙᴏᴛ ᴏᴡɴᴇʀ❏
 
 > *𝚠𝚑𝚊𝚝𝚜𝚊𝚙𝚙⇘* 
-https://wa.me/254112291443
+https://wa.me/254798757856
 
 > *𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖⇘* 
-https://www.instagram.com/richgagamidush
+https://www.instagram.com/i.c.o.n.i.c_n.i.c.c.u.r?igsh=MW5kM3NnZTVxYnY1bQ==
 
 > *𝚝𝚎𝚕𝚎𝚐𝚛𝚊𝚖⇘*
-t.me/richardgaga]
+https://t.me/+254798757856]
 
-> ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ʀɪᴄʜɢᴀɢᴀᴍɪᴅᴜs`
-	 await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id,{text:WASI_MD_TEXT},{quoted:session})
+> ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ICONICFREEZER`
+	 await Qr_Code_By_Freezer_Tech.sendMessage(Qr_Code_By_Freezer_Tech.user.id,{text:FREEZER_MD_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Wasi_Tech.ws.close();
+					await Qr_Code_By_Freezer_Tech.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
-					WASI_MD_QR_CODE();
+					FREEZER_MD_QR_CODE();
 				}
 			});
 		} catch (err) {
@@ -99,6 +99,6 @@ t.me/richardgaga]
 			await removeFile("temp/" + id);
 		}
 	}
-	return await WASI_MD_QR_CODE()
+	return await FREEZER_MD_QR_CODE()
 });
 module.exports = router
